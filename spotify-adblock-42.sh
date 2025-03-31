a71cc3ad04ba5807bffa2c3f853b21305cb5b8d7
princess-mikus <kiankocasaubon01@gmail.com>
2025-03-31 10:31:21 +0200

    Spotify test

---
+++ spotify-adblock-42.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.spotify-adblock/repo && git clone https://github.com/abba23/spotify-adblock.git ~/.spotify-adblock/repo && cd ~/.spotify-adblock/repo
+
+echo "Making..." && make
+
+if ! flatpak list | grep com.spotify.Client; then
+	if locale | grep language == LANGUAGE=es; then
+		echo "Spotify no instalado, instalalo primero"
+	else
+		echo "Spotify not installed, install it first"
+	fi	
+else
+	target/release/libspotifyadblock.so ~/.spotify-adblock/spotify-adblock.so
+	mkdir -p ~/.var/app/com.spotify.Client/config/spotify-adblock && cp config.toml ~/.var/app/com.spotify.Client/config/spotify-adblock
+	flatpak override --user --filesystem="~/.spotify-adblock/spotify-adblock.so" --filesystem="~/.config/spotify-adblock/config.toml" com.spotify.Client
+	echo "Success! Cleaning"
+
+fi
+	
